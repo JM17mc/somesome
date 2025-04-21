22496c2a81abf4d389b099af236a9da2b76d762e
JM17mc <jhnmchlmamac@gmail.com>
2025-04-21 20:36:29 +0800

    Add files via upload

---
+++ birthday.py
@@ -0,0 +1,10 @@
+
+def happy_birthday(x, y):
+    print(f"Happy birthday to {x}!")
+    print(f"You are {y} years old")
+    print("Happy birthday to you!")
+    print()
+
+happy_birthday("Man", 20)
+happy_birthday("Bro", 21)
+happy_birthday("Ate", 18)
