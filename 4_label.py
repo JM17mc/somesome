cc1c3061ee85946ccd73f21e03cc3b6f5775fa6b
JM17mc <jhnmchlmamac@gmail.com>
2025-05-22 08:02:27 +0800

    Add files via upload

---
+++ 4_label.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import *
+
+window = tk.Tk()
+window.geometry("300x300")
+window.title("ITCS103 TKINTER!")
+
+label = tk.Label(window, text="BSIT LANG MASARAP!")
+label.pack()
+
+window.mainloop()
+
